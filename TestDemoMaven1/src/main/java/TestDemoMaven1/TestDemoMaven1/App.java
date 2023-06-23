1cd46f88534434453e693f2fc9ac7b3d79ff534b
srikanthchincholi <srikanthchincholi@yahoo.com>
2023-06-23 09:49:58 +0530

    comitting changes

--- TestDemoMaven1/src/main/java/TestDemoMaven1/TestDemoMaven1/App.java
+++ TestDemoMaven1/src/main/java/TestDemoMaven1/TestDemoMaven1/App.java
@@ -2,14 +2,7 @@ package TestDemoMaven1.TestDemoMaven1;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
+	System.out.println("Stop printing Hello World these many times");
     System.out.println("Stop printing Hello World these many times");
 
  }
