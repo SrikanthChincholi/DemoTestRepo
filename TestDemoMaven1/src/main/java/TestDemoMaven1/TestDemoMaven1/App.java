3fd7d29e52226fd15e9026325de9fd5fab66766e
SrikanthChincholi <124526647+SrikanthChincholi@users.noreply.github.com>
2023-06-23 09:42:49 +0530

    Update App.java
    I have removed few unwanted stmt from file

--- TestDemoMaven1/src/main/java/TestDemoMaven1/TestDemoMaven1/App.java
+++ TestDemoMaven1/src/main/java/TestDemoMaven1/TestDemoMaven1/App.java
@@ -4,12 +4,6 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
     System.out.println("Stop printing Hello World these many times");
 
  }
