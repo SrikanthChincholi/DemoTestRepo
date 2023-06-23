a81490ac0764dda7dcfa275905a79088d38d90bb
SrikanthChincholi <124526647+SrikanthChincholi@users.noreply.github.com>
2023-06-23 09:49:17 +0530

    Update App.java
    removed stmts

--- TestDemoMaven1/src/main/java/TestDemoMaven1/TestDemoMaven1/App.java
+++ TestDemoMaven1/src/main/java/TestDemoMaven1/TestDemoMaven1/App.java
@@ -2,14 +2,6 @@ package TestDemoMaven1.TestDemoMaven1;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
-    System.out.println("Hello World!");
     System.out.println("Stop printing Hello World these many times");
 
  }
