c4b70d20e16c79f94089b8d6a1d0f397d8fdd8e8
SrikanthChincholi <124526647+SrikanthChincholi@users.noreply.github.com>
2023-06-23 10:01:39 +0530

    Update App.java

--- TestDemoMaven1/src/main/java/TestDemoMaven1/TestDemoMaven1/App.java
+++ TestDemoMaven1/src/main/java/TestDemoMaven1/TestDemoMaven1/App.java
@@ -4,13 +4,5 @@ public class App {
 	public static void main(String[] args) {
 
 		System.out.println("Stop printing Hello World these many times");
-		System.out.println("Stop printing Hello World these many times");
-		System.out.println("Stop printing Hello World these many times");
-		System.out.println("Stop printing Hello World these many times");
-			System.out.println("Stop printing Hello World these many times");
-		System.out.println("Stop printing Hello World these many times");
-		System.out.println("Stop printing Hello World these many times");
-		System.out.println("Stop printing Hello World these many times");
-
 	}
 }
