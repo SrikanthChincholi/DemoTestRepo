b119af1419f7bf508c0d50d9ce5864b72191116b
srikanthchincholi <srikanthchincholi@yahoo.com>
2023-06-26 10:40:30 +0530

    made changes added chrome browser and couple of tests to the code

--- TestDemoMaven1/src/test/java/TestDemoMaven1/TestDemoMaven1/AppTest.java
+++ TestDemoMaven1/src/test/java/TestDemoMaven1/TestDemoMaven1/AppTest.java
@@ -1,15 +1,71 @@
 package TestDemoMaven1.TestDemoMaven1;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class AppTest {
-    
-  @Test
-  public void shouldAnswerWithTrue() {
-	  System.out.println("This is a Maven Project ");
-    Assert.assertTrue(true);
-    System.out.println("This is a Maven Project ");
-    Assert.assertTrue(true);
-  }
+
+	static WebDriver d;
+
+	@BeforeMethod
+	public void openBrowser() {
+		d = new ChromeDriver();
+		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		d.manage().window().maximize();
+		d.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	}
+
+	@Test
+	public void loginTest1() {
+
+		d.findElement(By.xpath("//input[@name='username']")).sendKeys("admin");
+		d.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+		d.findElement(By.xpath("//button[@type='submit']")).click();
+		WebElement dashboard = d.findElement(By.xpath("//h6[text()='Dashboard']"));
+		try {
+			if (dashboard.isDisplayed()) {
+				Assert.assertTrue(true);
+				System.out.println("Login Success");
+			} else {
+				System.out.println("Login Failed");
+				Assert.assertTrue(false);
+			}
+		} catch (Exception e) {
+			System.out.println("Login Failed");
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void loginTest2() {
+
+		d.findElement(By.xpath("//input[@name='username']")).sendKeys("admin123");
+		d.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+		d.findElement(By.xpath("//button[@type='submit']")).click();
+		WebElement dashboard = d.findElement(By.xpath("//h6[text()='Dashboard']"));
+		try {
+			if (dashboard.isDisplayed()) {
+				Assert.assertTrue(true);
+				System.out.println("Login Success");
+			} else {
+				System.out.println("Login Failed");
+				Assert.assertTrue(false);
+			}
+		} catch (Exception e) {
+			System.out.println( e.getMessage() + "Login Failed");
+		}
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		d.quit();
+	}
 }
